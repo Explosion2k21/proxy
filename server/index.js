dd6b7175ccd9fd983a9d9ba0efc5a9667fd11af2
ZribiHaythem <zribihaythem@gmail.com>
2021-03-04 11:40:07 +0100

    deployy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
   This file is responsible for requiring your express server and then binding it to the desired port
 */
 
-const server = require('./server.js');
+const server = require("./server.js");
 
 const PORT = 3000 || process.env.PORT;
 
