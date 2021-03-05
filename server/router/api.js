e05acc79eee394c13df63d5613b09908a6108de0
ZribiHaythem <zribihaythem@gmail.com>
2021-03-05 21:23:39 +0100

    last serv

--- server/router/api.js
+++ server/router/api.js
@@ -4,7 +4,7 @@ const {
   service1,
   service2,
   service3,
-  // service4,
+  service4,
 } = require("../config/services.js");
 
 const router = Router();
@@ -15,9 +15,9 @@ router.use(
   createProxyMiddleware({ target: service2.url, changeOrigin: true })
 );
 router.use(service3.api, createProxyMiddleware({ target: service3.url, changeOrigin: true }));
-// router.use(
-//   service4.api,
-//   createProxyMiddleware({ target: service4.url, changeOrigin: true })
-// );
+router.use(
+  service4.api,
+  createProxyMiddleware({ target: service4.url, changeOrigin: true })
+);
 
 module.exports = router;
